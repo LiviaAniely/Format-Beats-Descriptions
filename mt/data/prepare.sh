9a29a0af9095381f9fa058a18aeb35cd8df38192
Livia Aniely <livia.almeida@ccc.ufcg.edu.br>
2026-06-24 11:21:32 -0300

    chore: just make the downloads and process the data if it wasnt already made

--- mt/data/prepare.sh
+++ mt/data/prepare.sh
@@ -1,13 +1,49 @@
 # From CTQScorer (Kumar et al., 2023)
 # https://github.com/AI4Bharat/CTQScorer/blob/master/dataset/prepare_datasets.sh
 
-# Download datasets - Europarl, Paracrawl
-wget https://www.statmt.org/europarl/v10/training/europarl-v10.fr-en.tsv.gz
-wget https://www.statmt.org/europarl/v10/training/europarl-v10.de-en.tsv.gz
-wget https://s3.amazonaws.com/web-language-models/paracrawl/bonus/en-ru.txt.gz
+# Check if the final extracted training files already exist
+if [ -f de/train.de ] && [ -f fr/train.fr ] && [ -f ru/train.ru ]; then
+    echo "Training datasets are already extracted and prepared. Skipping download and extraction."
+    exit 0
+fi
 
-# Extract data
-gzip -d europarl-v10.fr-en.tsv.gz
-gzip -d europarl-v10.de-en.tsv.gz
-gzip -d en-ru.txt.gz
-python extract.py
+# Download datasets only if neither the decompressed tsv/txt nor the compressed .gz files exist
+if [ ! -f europarl-v10.fr-en.tsv ] && [ ! -f europarl-v10.fr-en.tsv.gz ]; then
+    echo "Downloading europarl-v10.fr-en.tsv.gz..."
+    wget https://www.statmt.org/europarl/v10/training/europarl-v10.fr-en.tsv.gz
+fi
+
+if [ ! -f europarl-v10.de-en.tsv ] && [ ! -f europarl-v10.de-en.tsv.gz ]; then
+    echo "Downloading europarl-v10.de-en.tsv.gz..."
+    wget https://www.statmt.org/europarl/v10/training/europarl-v10.de-en.tsv.gz
+fi
+
+if [ ! -f en-ru.txt ] && [ ! -f en-ru.txt.gz ]; then
+    echo "Downloading en-ru.txt.gz..."
+    wget https://s3.amazonaws.com/web-language-models/paracrawl/bonus/en-ru.txt.gz
+fi
+
+# Decompress files if the .gz files exist and we need the raw files
+if [ -f europarl-v10.fr-en.tsv.gz ]; then
+    echo "Decompressing europarl-v10.fr-en.tsv.gz..."
+    gzip -d europarl-v10.fr-en.tsv.gz
+fi
+
+if [ -f europarl-v10.de-en.tsv.gz ]; then
+    echo "Decompressing europarl-v10.de-en.tsv.gz..."
+    gzip -d europarl-v10.de-en.tsv.gz
+fi
+
+if [ -f en-ru.txt.gz ]; then
+    echo "Decompressing en-ru.txt.gz..."
+    gzip -d en-ru.txt.gz
+fi
+
+# Run python extraction script
+if [ -f europarl-v10.fr-en.tsv ] && [ -f europarl-v10.de-en.tsv ] && [ -f en-ru.txt ]; then
+    echo "Extracting datasets..."
+    python extract.py
+else
+    echo "Error: TSV/TXT files for extraction not found!"
+    exit 1
+fi
--- mt/src/prepare.sh
+++ mt/src/prepare.sh
@@ -1,14 +1,39 @@
 # dependency parsing for Polynomial
-# python parse.py
+if [ -f ../data/de/train.de.spacy.json ] && [ -f ../data/fr/train.fr.spacy.json ] && [ -f ../data/ru/train.ru.spacy.json ]; then
+    echo "SpaCy parse files already exist. Skipping parse.py."
+else
+    echo "Running parse.py..."
+    python parse.py
+fi
 
 # generate random examples
-# python rand_example.py
+if [ -f ../data/de/index/test/into/rand1.index ] && [ -f ../data/fr/index/test/into/rand1.index ] && [ -f ../data/ru/index/test/into/rand1.index ]; then
+    echo "Random examples index files already exist. Skipping rand_example.py."
+else
+    echo "Running rand_example.py..."
+    python rand_example.py
+fi
 
 # get BM25 examples
-python bm25.py
+if [ -f ../data/de/index/test/into/bm25.index ] && [ -f ../data/fr/index/test/into/bm25.index ] && [ -f ../data/ru/index/test/into/bm25.index ]; then
+    echo "BM25 index files already exist. Skipping bm25.py."
+else
+    echo "Running bm25.py..."
+    python bm25.py
+fi
 
 # get Polynomial examples
-python polynomial.py
+if [ -f ../data/de/index/test/into/polynomial.index ] && [ -f ../data/fr/index/test/into/polynomial.index ] && [ -f ../data/ru/index/test/into/polynomial.index ]; then
+    echo "Polynomial index files already exist. Skipping polynomial.py."
+else
+    echo "Running polynomial.py..."
+    python polynomial.py
+fi
 
 # generate random nouns
-python rand_noun.py
+if [ -f noun_65536.txt ]; then
+    echo "Random nouns file already exists. Skipping rand_noun.py."
+else
+    echo "Running rand_noun.py..."
+    python rand_noun.py
+fi
