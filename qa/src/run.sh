9aa9237b2c2b3285e54567c619658f143ca254b7
Livia Aniely <livia.almeida@ccc.ufcg.edu.br>
2026-07-02 17:27:04 -0300

    feat: add vanilla template on run.sh for qa

--- qa/src/run.sh
+++ qa/src/run.sh
@@ -3,7 +3,7 @@ python qa_inference.py \
     --dataset csqa strategyqa date sports logicalfallacy threeobjects knownunknowns gsm8k aqua \
     --shot 4 \
     --batch_size 4 \
-    --templates ensemble_random \
+    --templates vanilla ensemble_random \
     --cot_mode 2 \
     --max_new_tokens 256 \
     --models alpaca llama2 mistral
